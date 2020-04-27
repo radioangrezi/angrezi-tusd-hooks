69b4a5dff2af55a04e382376116954c8bb2f2735
Leo <130.0x82@gmail.com>
2020-04-27 23:23:51 +0200

    remove enforce audio hook

--- hooks/enforce-audio-mimetype.js
+++
@@ -1,9 +0,0 @@
-
-module.exports = (tusdBody) => {
-    console.log("messagebody", tusdBody);
-    const { HTTPRequest, Upload } = tusdBody;
-    console.log(HTTPRequest, Upload);
-    return {
-        statusCode: 200
-    };
-}
--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const tusdHookServer = require("./server");
 const importIntoLibreTimeHook = require("./hooks/import-into-libretime");
-const enforceAudioMimeType = require("./hooks/enforce-audio-mimetype");
 
-tusdHookServer.on("pre-create", importIntoLibreTimeHook);
+// tusdHookServer.on("pre-create", importIntoLibreTimeHook);
 tusdHookServer.on("post-receive", importIntoLibreTimeHook);
 tusdHookServer.start();
