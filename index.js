40b33d03fcadbd8294307991d186140c26a744ef
Leo <130.0x82@gmail.com>
2020-05-14 14:06:51 +0200

    verbose logging & add hook identifier for better logging

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const tusdHookServer = require("./server");
 const importIntoLibreTimeHook = require("./hooks/import-into-libretime");
+const enforceFileType = require("./hooks/check-file-type");
 
-// tusdHookServer.on("pre-create", importIntoLibreTimeHook);
-tusdHookServer.on("post-finish", importIntoLibreTimeHook);
+tusdHookServer.on("pre-create", "EnforceAudioType", enforceFileType);
+tusdHookServer.on("post-finish", "LibretimeProcessing", importIntoLibreTimeHook);
 tusdHookServer.start();
--- server.js
+++ server.js
@@ -12,6 +12,7 @@ const argv = yargs.option("port", {
 
 const port = argv.port || 3001;
 
+// Map(key: string "hookname", value: [string "hookIdentifier", function "hookFunction"])
 const hookMap = new Map();
 
 const AVAILABLE_HOOKS = [
@@ -36,21 +37,21 @@ const getFromArrayMap = (aMap, key) => {
   return arr;
 }
 
-const handleHookError = (res) => {
-  console.log("A hook exited with an error");
+const handleHookError = (hid, res, message) => {
+  console.log(`[Hook: ${hid}] Exited with error: ${message}`);
   res.status(500).send("A hook exited with an error");
 }
-const handleBadReturnFromHookError = (res) => {
-  console.log("A hook returned bad data");
+const handleBadReturnFromHookError = (hid, res) => {
+  console.log(`[Hook: ${hid}] Bad return data`);
   res.status(500).send("A hook returned bad data");
 }
-const handleBadRequestError = (res) => {
-  console.log("Bad request");
+const handleBadRequestError = (hid, res) => {
+  console.log(`[Hook: ${hid}] Bad request from tusd`);
   res.status(400).send("Bad request");
 }
 
-const handleSuccess = (res) => {
-  console.log("Hook processed successfully");
+const handleSuccess = (hid, res) => {
+  console.log(`[Hook: ${hid}] Run successfully`);
   res.status(200).send();
 }
 
@@ -64,30 +65,29 @@ const server = {
     app.post("/", async (req, res) => {
       const hookName = req.header("Hook-Name");
       const { body } = req;
-      if (!hookName) return handleBadRequestError(res);
-      if (!isValidHookName(hookName)) return handleBadRequestError(res);
+      if (!hookName) return handleBadRequestError(hookName, res);
+      if (!isValidHookName(hookName)) return handleBadRequestError(hookName, res);
       const hooks = getFromArrayMap(hookMap, hookName);
-      await Promise.all(hooks.map(async (hookFunction) => {
+      await Promise.all(hooks.map(async ([hookIdentifier, hookFunction]) => {
         try {
           const { statusCode, message } = await hookFunction(body);
           if (!statusCode) return handleBadReturnFromHookError(res);
           if (statusCode != 200 && !message) return handleBadReturnFromHookError(res);
         } catch (error) {
-          console.error("Error in Hook: ", error.message)
-          return handleHookError(res);
+          return handleHookError(res, error.message);
         }
       }));
-      return handleSuccess();
+      return handleSuccess(res);
     });
 
     app.listen(port, "localhost", () => {
       console.log(`Angrezi Tusd Hook Server listening on port ${port}!`);
     });
   },
-  on: (hookName, hookFunction) => {
+  on: (hookName, hookIdentifier, hookFunction) => {
     if (!isValidHookName(hookName)) throw new Error(`Hook "${hookName}" not supported`);
-    console.log(`Registered function for hook "${hookName}"`);
-    addToArrayMap(hookMap, hookName, hookFunction);
+    console.log(`Registered function "${hookIdentifier}"for hook "${hookName}"`);
+    addToArrayMap(hookMap, hookName, [hookIdentifier, hookFunction]);
   },
 };
 
